1efb87be0681787dc88005d724198f978bda3c15
Dale de Kock <ddk2707@gmail.com>
2022-03-18 15:51:37 +0200

    contact, footer and nav populated

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="row">
+      <div class="col-sm-6">
+        <img src="" alt="">
+      </div>
+      <div class="col-sm-6">
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  width: 100%;
+}
+</style>
